2f7c8e8ff3982b6a876a92ae85f8a3c9d27c96df
Gustavo Schaefer <gstvo.schaefer@gmail.com>
2020-07-31 21:23:16 -0300

    resize images and store logs in .txt file

---
+++ resize-and-store/resize-images-opencv.py
@@ -0,0 +1,19 @@
+import cv2 
+import os
+import glob
+
+input_dir = "C:/Users/Lucimara Santos/Pictures/Tolkien"
+new_dir = "C:/RESIZE"
+new_size = (1920, 1080)
+
+if os.path.isdir(new_dir) == False:
+	os.mkdir(new_dir) 
+
+folderlen = len(input_dir)
+for img in glob.glob(input_dir + "/*.jpg"):
+	image = cv2.imread(img)
+	image = cv2.resize(image, new_size)
+	cv2.imwrite(new_dir + img[folderlen:], image)
+	cv2.imshow('image', image)
+	cv2.waitKey(50)
+cv2.destroyAllWindows()
